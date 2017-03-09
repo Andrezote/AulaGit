b5938eaf4680b103679dcbc3071773ae68f524bf
Andre Tosetto <Andre Tosetto@Andre-PC.home>
2017-03-09 14:31:26 -0300

    Ola Mundo versao 2

--- ProjetoVersionado/src/br/univel/teste/Principal.java
+++ ProjetoVersionado/src/br/univel/teste/Principal.java
@@ -3,7 +3,8 @@ package br.univel.teste;
 public class Principal {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		String msg = "Ola Mundo";
+		System.out.println(msg);
 	}
 
 }
